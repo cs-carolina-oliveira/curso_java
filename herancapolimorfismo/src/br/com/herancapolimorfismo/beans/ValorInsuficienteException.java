926131b367066e99f6a1da5c3aed8e1b427604ff
Carolina Oliveira <carolina.oliveira@concrete.com.br>
2016-08-29 14:37:45 -0300

    Executa novas funcionalidades
    Executa saque e set limite, e imprime na tela dados do cliente juntamente com seu saldo.
    Cria lista de novas contas e imprime ordenadamente os clientes.
    Calcula area de circulo
    Lanca exception quando necessario

--- herancapolimorfismo/src/br/com/herancapolimorfismo/beans/ValorInsuficienteException.java
+++
@@ -1,7 +0,0 @@
-package br.com.herancapolimorfismo.beans;
-
-public class ValorInsuficienteException extends RuntimeException {
-    public ValorInsuficienteException(double valor) {
-        super("Valor inválido " + valor);
-    }
-}
